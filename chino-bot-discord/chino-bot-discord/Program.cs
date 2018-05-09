04d0100378acfa57a46752e815a3fdd1769c5cac
Peijun/ページュン <contact.zouri.music@gmail.com>
2018-05-09 21:08:38 +0900

    make:c# solution

---
+++ chino-bot-discord/chino-bot-discord/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chino_bot_discord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
